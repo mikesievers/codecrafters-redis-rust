df645bf53a51cc7cca55ba49aeb2119d0b9afa0d
Michael Sievers <michael@hamburg.de>
2026-06-07 18:25:15 +0000

    Move to dev container, read lines from stream

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::TcpListener,
+};
 
-fn main() {
+use anyhow::{Error, Result};
+
+fn main() -> Result<(), Error> {
     println!("Starting sort-of Redis.");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
-                let _ = stream.write_all("+PONG\r\n".as_bytes());
+
+                let mut writer = stream.try_clone()?;
+                let reader = BufReader::new(&stream);
+
+                for line in reader.lines() {
+                    match line {
+                        Ok(_) => {
+                            let _ = writer.write_all("+PONG\r\n".as_bytes());
+                        }
+                        Err(_) => {
+                            println!("End of stream.");
+                        }
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    Ok(())
 }

f2d35a229c8de609cb10d9909f6695dc9257da5e
Michael Sievers <michael@hamburg.de>
2026-08-07 09:28:15 +0000

    Rewrite llen

--- src/command/cmd_llen.rs
+++ src/command/cmd_llen.rs
@@ -8,20 +8,16 @@ pub struct CommandLlen {}
 
 impl Command for CommandLlen {
     fn execute(&self, db: &dyn Db, args: &[Resp]) -> Resp {
-        let resp_zero = Resp::Int(0);
-
-        if args.is_empty() {
-            return resp_zero;
-        }
-        match &args[0] {
-            Resp::BulkString(key) => match db.get(key) {
-                Some(entry) => match entry.value {
-                    RedisType::List(items) => Resp::Int(items.len() as i64),
-                    _ => resp_zero,
-                },
-                None => resp_zero,
-            },
-            _ => resp_zero,
-        }
+        args.first()
+            .and_then(|arg| match arg {
+                Resp::BulkString(key) => Some(key),
+                _ => None,
+            })
+            .and_then(|key| db.get(key))
+            .and_then(|entry| match entry.value {
+                RedisType::List(items) => Some(Resp::Int(items.len() as i64)),
+                _ => None,
+            })
+            .unwrap_or(Resp::Int(0))
     }
 }
